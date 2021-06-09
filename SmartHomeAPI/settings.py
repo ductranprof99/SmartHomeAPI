35590f39cfe825ca218e02a2d9c786ea7b346032
wonrax <hahuylonghai2012@gmail.com>
2021-06-09 21:18:39 +0700

    change token expiration time

--- SmartHomeAPI/settings.py
+++ SmartHomeAPI/settings.py
@@ -156,8 +156,8 @@ AUTH_USER_MODEL = 'API.User'
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 
