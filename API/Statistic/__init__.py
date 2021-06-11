d768ba46ec16e51fe07c6a86ef53185767fc0849
ductranprof99 <duc.tranhvhk@hcmut.edu.vn>
2021-06-11 11:11:35 +0700

    fix link

--- API/Statistic/__init__.py
+++ API/Statistic/__init__.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('statistics?device-type=light&range=week',views.StatisticManager.as_view()),
+    path('statistics',views.StatisticManager.as_view()),
 ]
